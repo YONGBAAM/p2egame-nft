612c21b21c83217bac865a3eb6515e778822294e
YONGBAAM <jokester59@naver.com>
2022-04-25 22:24:04 +0900

    follow setting in lecture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
-import React, {FC} from 'react'
+import React, { FC, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import NftStorage from './routes/nft-storage';
 
-const App :FC = () => {
+const App: FC = () => {
+  const [account, setAccount] = useState<string>("");
+
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Layout onConnected= {setAccount} account = {account}>
+        <Routes>
+          <Route path = "/" element = {<div>main</div>} />
+          <Route path = "nft-storage" element = {<NftStorage account = {account} />}  />
+          </Routes>
+      </Layout>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+
+interface Window {
+    ethereum: any; // 이더리움이란 오브젝트를 리액트가 인식하게 타입을 잡아줌. 
+  }
+  
