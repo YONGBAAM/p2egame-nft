e2683332f89c588c53ae2dd335c5d23ad9d40813
YONGBAAM <jokester59@naver.com>
2022-05-02 20:21:58 +0900

    add build

---
+++ frontend/build/precache-manifest.9eb81d378b0c200d478c0e1758a95c99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01071790b6538f4dabe9",
+    "url": "/static/js/main.0890a9ff.chunk.js"
+  },
+  {
+    "revision": "c2aced39f35e6e068315",
+    "url": "/static/js/runtime~main.985837af.js"
+  },
+  {
+    "revision": "c59145371f9cd2ee69cd",
+    "url": "/static/js/2.a5872c05.chunk.js"
+  },
+  {
+    "revision": "55cc66814b14b358ab6d",
+    "url": "/static/js/3.4b5d4cfd.chunk.js"
+  },
+  {
+    "revision": "85c9259788e18b53a07b6d95ea0b6e0c",
+    "url": "/index.html"
+  }
+];
